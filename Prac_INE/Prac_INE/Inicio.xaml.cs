b51e9ba094a4dd3f0a68501cf994732295e05bb1
rache <rache@RAQUEL>
2025-04-05 19:31:02 -0600

    Agregar archivos de proyecto.

---
+++ Prac_INE/Prac_INE/Inicio.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prac_INE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int edad = Int32.Parse(txtEdad.Text);
+
+            if (edad < 18)
+            {
+                Navigation.PushAsync(new Menor());
+            }
+            else
+            {
+                Navigation.PushAsync(new INE());
+            }
+        }
+    }
+}
---
+++ Prac_INE/Prac_INE/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prac_INE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int edad = Int32.Parse(txtEdad.Text);
+
+            if (edad < 18)
+            {
+                Navigation.PushAsync(new Menor());
+            } else
+            {
+                Navigation.PushAsync(new INE());
+            }
+        }
+    }
+}
